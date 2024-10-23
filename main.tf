376deaf918202e2b4026d63c59aa14ee66cb644a
Deepak8087 <Chavandeepak2011@gmail.com>
2024-10-23 14:41:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,4 @@ resource "aws_instance" "one" {
   count         = 2
   ami           = "ami-050cd642fd83388e4"
   instance_type = "t2.micro"
-  
-  tags = {
-    Name = "${terraform.workspace}-server"
-  }
 }
